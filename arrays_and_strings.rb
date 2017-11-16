818d3501b7a2d4d1917ebaf8cad06e24d8ea0c0d
= <pjfritch1@gmail.com>
2017-11-15 18:48:59 -0800

    unique method

--- arrays_and_strings.rb
+++ arrays_and_strings.rb
@@ -1,2 +1,8 @@
 # Implement an algorithm to determine if a string has all unique characters. What if you
 # can not use additional data structures?
+
+def unique?(str)
+  seen = ""
+  str.each_char do |chr|
+  end
+end
